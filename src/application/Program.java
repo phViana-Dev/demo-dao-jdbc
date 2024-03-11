86ab64b1f1eb5118e072d21686b70ba32cda676c
Pedro Viana <dev.pedroviana@gmail.com>
2024-03-11 11:28:51 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,10 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        // dessa forma o programa não conhece a implementação, só a interface.
+        // é também uma forma de injetar dependências sem explicitar a implementação
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Seller;
+
+public class DaoFactory {
+
+    // Dessa forma, a classe vai expôr um método que retorna o tipo da interface, mas internamente vai instanciar uma implementação
+    // é um macete para não precisar expôr a implementação
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+}
