551a4a104264560e7f17ecd58d4c7bc3e35f7a6c
Pedro Viana <dev.pedroviana@gmail.com>
2024-03-12 11:19:18 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,6 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
-import java.util.function.DoublePredicate;
 
 public class Program {
 
@@ -33,5 +32,10 @@ public class Program {
         for (Seller obj: list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
